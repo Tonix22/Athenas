eee3f224e94fe371cfd653d26081916e1feda983
Luis Fernando <luis.fernando.espinosa.perez@hp.com>
2020-02-12 16:22:33 -0600

    BubbleSort pt.2.1
    even prettier!

--- C_CPP_Proyects/BubbleSort.cpp
+++ C_CPP_Proyects/BubbleSort.cpp
@@ -1,26 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-//#include <cstdlib>
 #include <stdio.h>
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
-    int aux;
-    
     int array[10]={1, 0, 2, 9, 3, 8, 4, 7, 5, 6};
+    int aux;
 
     for(int x=1; x<sizeof(array)/sizeof(array[0]); x++){
         if(array[x]<array[x-1]){
-
             for(int y=x; y>0; y--){
                 if(array[y]<array[y-1]){
                     aux=array[y];
@@ -31,6 +19,7 @@ int main(int argc, char** argv) {
         }
     }
 
+    //Print
     for(int i=0; i<sizeof(array)/sizeof(array[0]); i++){
         printf("%d ", array[i]);
     }
