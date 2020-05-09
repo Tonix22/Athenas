6b0f28f16e9be6243415229126042adb7e6bcd97
Tonix <emiliotonix@gmail.com>
2020-05-08 21:40:59 -0500

    enchaced terminal

--- C_CPP_Proyects/APPS/SerialMonitor/Windows/serial.cpp
+++ C_CPP_Proyects/APPS/SerialMonitor/Windows/serial.cpp
@@ -1,8 +1,13 @@
+#include <thread> 
 #include "serial.hpp"
 #include <unordered_map>
 #include <cstdlib>
+#include <windows.h>
+#include <tchar.h>
+
 using namespace std;
 unordered_map<int,string> commands;
+bool can_read = false;
 void init_commands(void)
 {
     //commands.insert(make_pair(KEY_UP,"KB_SIM UP\r\n"));
@@ -13,20 +18,57 @@ void init_commands(void)
     commands[QUIT]      = "Quit";
 }
 
-void keyboardController(SerialPort* ser)
+static DWORD WINAPI keyboardController( LPVOID lpParam ) 
 {
     string cmd_check;
+    SerialPort* ser = ( SerialPort*)lpParam;
     while(cmd_check !="Quit")
     {
         cmd_check = commands[getch()];
         ser->write_string(cmd_check);
+        can_read = true;
+    }
+    delete(ser);
+    return 0;
+}
+static DWORD WINAPI Recieve_Thread(LPVOID lpParam)
+{
+    SerialPort* ser = ( SerialPort*)lpParam;
+    while(1)
+    {
+        if(can_read == true)
+        {
+            ser->read_string();
+            can_read = false;
+        }
     }
 }
+
+void create_serial_threads(SerialPort* serial_handler)
+{
+    HANDLE  tx_handle;
+    DWORD   tx_ID;
+
+    HANDLE  rx_handle;
+    DWORD   rx_ID;
+
+    THREAD_CREATE(tx_handle,tx_ID,keyboardController);
+    THREAD_CREATE(rx_handle,rx_ID,Recieve_Thread);
+    // Wait until all threads have terminated.
+    WaitForSingleObject(tx_handle,INFINITE);
+    
+    CloseHandle(tx_handle);
+    CloseHandle(rx_handle);
+
+}
+
+
 int main(int argc, char const *argv[])
 {
     SerialPort* ser= (argv[1]) ? (new SerialPort(argv[1], atoi(argv[2]))) : new SerialPort("COM27", 115200);
     init_commands();
-    keyboardController(ser);
-    delete(ser);
+    //thread first  (keyboardController,ser); 
+    //thread second (bar,ser);
+    create_serial_threads(ser);
     return 0;
 }
--- C_CPP_Proyects/APPS/SerialMonitor/Windows/serial.hpp
+++ C_CPP_Proyects/APPS/SerialMonitor/Windows/serial.hpp
@@ -8,6 +8,12 @@
 #define KEY_RIGHT 77
 #define QUIT 113
 
+#define THREAD_CREATE(hand,id,func)  hand = CreateThread(NULL, 0, func, (void*)serial_handler,0,&id);\
+                                if (hand == NULL)\
+                                {\
+                                    ExitProcess(3);\
+                                }\
+
 using namespace std;
 class SerialPort
 {
@@ -48,27 +54,61 @@ class SerialPort
         }
 
         COMMTIMEOUTS timeouts={0};
-        timeouts.ReadIntervalTimeout=50;
-        timeouts.ReadTotalTimeoutConstant=50;
-        timeouts.ReadTotalTimeoutMultiplier=10;
-        timeouts.WriteTotalTimeoutConstant=50;
-        timeouts.WriteTotalTimeoutMultiplier=10;
+        timeouts.ReadIntervalTimeout=1;
+        timeouts.ReadTotalTimeoutConstant=1;
+        timeouts.ReadTotalTimeoutMultiplier=1;
+        timeouts.WriteTotalTimeoutConstant=1;
+        timeouts.WriteTotalTimeoutMultiplier=1;
         if(!SetCommTimeouts(hSerial, &timeouts))
         {
             std::cout << "set timeout error" << std::endl;
         }
     }
+    int read_string()
+    {
+        DCB dcb; 
+        int retVal;
+        BYTE Byte[100]; 
+        DWORD dwBytesTransferred;
+        DWORD dwCommModemStatus;
+        memset(Byte,0,100);
+        if (!GetCommState(hSerial,&dcb))
+        {
+            cout<<"COM state NOT OK "<<endl;
+        }
+        
+        dcb.BaudRate = 115200; //9600 
+        dcb.ByteSize = 8; //8 
+        dcb.Parity   = NOPARITY; //no 
+        dcb.StopBits = ONESTOPBIT; //1 stop 
+        if (!SetCommState(hSerial,&dcb))
+        {
+            cout<<"CON SET NOT OK"<<endl;
+            return 0x100; 
+        }
+        SetCommMask (hSerial, EV_RXCHAR | EV_ERR); //receive character event 
+        WaitCommEvent (hSerial, &dwCommModemStatus, 0); //wait for character
+
+        if (dwCommModemStatus & EV_RXCHAR) 
+        {
+            ReadFile (hSerial, &Byte, 100, &dwBytesTransferred, 0); //read 1 
+        }
+        else if (dwCommModemStatus & EV_ERR) 
+        {
+            retVal = 0x101; 
+        }
+        //retVal = Byte; 
+        printf("byte: %s\r\n",Byte);
+        return retVal;
+    }
     bool write_string(string& message)
     {
         DWORD dwBytesRead = 0;
+        DWORD read = 0;
         bool status = WriteFile(hSerial, message.c_str(), message.size(), &dwBytesRead, NULL);
         if(status == false)
         {
             std::cout << "write error" << std::endl;
-        }
-        else
-        {
-            std::cout << message << std::endl;
         }
         return status;
     }
