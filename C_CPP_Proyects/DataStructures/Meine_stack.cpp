5365889eff5f674d04bc6edaa9d37025c21b79c1
Tonix22 <emiliotonix@gmail.com>
2020-07-22 13:01:11 -0500

    Meiene stack

---
+++ C_CPP_Proyects/DataStructures/Meine_stack.cpp
@@ -0,0 +1,35 @@
+#include <stack>
+using namespace std;
+class MyStack
+{
+    public: 
+    bool Empty() const 
+    {
+
+    }
+    int pop()
+    {
+
+    }
+    void push()
+    {
+
+    }
+    private:
+    stack<int> element;
+    struct MaxWithCount
+    {
+        int max;
+        int count;
+    };
+    stack<MaxWithCount> cached_elements;
+};
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
