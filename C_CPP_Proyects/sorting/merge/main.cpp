e695b8a8a47359492c23f051804a74b4d4f0d4bc
Tonix22 <emiliotonix@gmail.com>
2020-09-29 00:59:39 -0500

    more detailed frog

--- C_CPP_Proyects/sorting/merge/main.cpp
+++ C_CPP_Proyects/sorting/merge/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <bits/stdc++.h>
+
 int* split(int arr[],int size)
 {
     int mid2 = size/2;
@@ -11,34 +12,37 @@ int* split(int arr[],int size)
 
     if(mid2!=0)
     {
+        // copias temporales
         memcpy(a,arr,mid1*sizeof(int));
         memcpy(b,arr+mid2,mid2*sizeof(int));
 
-        split(a,mid2); 
-        split(b,mid1);
+        split(a,mid2); // Has esto con papel amigo
+        split(b,mid1); // Igual para esto
 
+        //ALTO!! 
+        //Aqui solo vamos a llegar cuando 
+        // la ranita tenga dos hojitas para brincar
+        // como minimo.
         int* rana   = &a[0];
         int* brinca = &b[0];
         int* nueva_base;
         int  cnt    = 0;
-        
+
         while(cnt < size-1)
         {
             if((*rana) > (*brinca))
             {
                 arr[cnt] = *brinca; // la rana no quiso brinca a este bloque y se cae
-                cnt++;              // se llena es bloque con lo que se cayo
                 brinca++;           // la rana brinca para al siguiente bloque
             }
             else
             {
                 arr[cnt] =  *rana; // se cae donde estaba la rana
-                cnt++; // se llena es bloque con lo que se cayo
-
                 nueva_base = brinca; //la ranita brinco tenemos nueva base 
                 brinca = rana+1; // nueva posición donde la rana brincara
                 rana   = nueva_base; // la rana brinca a un lugar seguro!!
             }
+            cnt++; // se lleno el bloque que se cayo, vamos al siguiente bloque!!!
         }
         arr[cnt] = *rana; // la rana se fue a mimir
         
@@ -49,8 +53,6 @@ int* split(int arr[],int size)
         return arr;
     }
 }
-
-
 
 int main(int argc, char const *argv[])
 {
