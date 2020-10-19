c13fb6a7ea213b89f2cfa9551122faf5e3dff487
Tonix22 <emiliotonix@gmail.com>
2020-10-19 05:29:11 +0000

    testing redblack tree

--- C_CPP_Proyects/DataStructures/redblacktree/redblack.py
+++ C_CPP_Proyects/DataStructures/redblacktree/redblack.py
@@ -5,6 +5,11 @@ from ctypes import *
 import pathlib
 import sys
 from enum import IntEnum
+import matplotlib.pyplot as plt
+import math
+from random import *
+import time
+
 
 # Define the types we need.
 class CtypesEnum(IntEnum):
@@ -75,7 +80,7 @@ class RedBlackTree:
             print("")
             return
         self.InorderRecursive(root.left.contents)
-        print(root.key, end=" ")
+        print(root.key, end=", ")
         self.InorderRecursive(root.right.contents)
 
     def PreorderRecursive(self,root:node)->None:
@@ -85,31 +90,53 @@ class RedBlackTree:
         self.PreorderRecursive(root.left.contents)
         print(root.key, end=" ")
         self.PreorderRecursive(root.right.contents)
-    """
-    def __del__(self):
-        for n in range(0,self.node_counter):
-            self.c_lib.destroy_node()
-    """
+    
+    def create_tree(self):
+        #create tree and nil
+        self.nil = pointer(node())
+        self.c_lib.create_node(byref(self.nil),0)
+        self.nil.contents.color = Paint.Black
+        #init tree
+        self.tree = tree_t()
+        self.tree.nil  = self.nil
+        self.tree.root = self.nil
+        #node counter
+        self.node_counter = 0
+
+    def DeleteALL(self,root:node)->None:
+        z = pointer(root)
+        if(root.key == 0):
+            return
+        print(root.key)
+        self.DeleteALL(root.left.contents)
+        self.DeleteALL(root.right.contents)
+        self.c_lib.destroy_node(byref(z))
+       
+        root.left  = None
+        root.right = None
+        root.p     = None
 
 if __name__ == "__main__":
-
-    Tree  = RedBlackTree()
     
-    Tree.insert(7)
-    Tree.insert(5)
-    Tree.insert(8)
-    Tree.insert(1)
-    Tree.insert(2)
-    Tree.insert(14)
-    Tree.insert(15)
-    Tree.insert(11)
-    Tree.insert(4)
-    Tree.insert(9)
+    items = 1
+    for n in range (0,32):
+        Tree  = RedBlackTree()
+        start_time = time.time()
+        for m in range(0,items):
+            Tree.insert(randint(1, 100))
+        print("--- %s seconds ---" % (time.time() - start_time))
+        items = 1<<n
+        p = Tree.tree.root.contents
+        #Tree.InorderRecursive(p)
+        #print("*"*30)
+        #p = Tree.tree.root.contents
+        #Tree.DeleteALL(p)
 
-    p = Tree.tree.root.contents
-
-    Tree.InorderRecursive(p)
-
+    
+    #x = np.arange(0, math.pi*2, 0.05)
+    #y = np.sin(x)
+    #plt.plot(x,y)
+    #plt.show()
     #print(Tree.tree.root.contents.key)
     #print(Tree.tree.root.contents.right.contents.key)
     #print(Tree.tree.root.contents.left.contents.key)
