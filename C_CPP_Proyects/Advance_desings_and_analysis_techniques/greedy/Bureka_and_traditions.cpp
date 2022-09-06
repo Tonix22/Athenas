b7630ef32f78e93b2916c39568fff0d7b4c33a1b
Tonix22 <emiliotonix@gmail.com>
2022-09-05 21:58:15 -0500

    Bureka

--- C_CPP_Proyects/Advance_desings_and_analysis_techniques/greedy/Bureka_and_traditions.cpp
+++ C_CPP_Proyects/Advance_desings_and_analysis_techniques/greedy/Bureka_and_traditions.cpp
@@ -1,65 +1,26 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <unordered_set>
 using namespace std;
 
-void print(int arr[], int n)
+// Not completly optimized but work
+void bureka_algorithm() 
 {
-    for(int i = 0; i < n; ++i)
-    {
-        cout << arr[i] << " ";
-    }
-    
-    cout << endl;
-}
-
-
-void func();
-int main(int argc, char const *argv[])
-{       
-    //int set7[] = {27,27,34,32,2,31,23,56,52,4}; // 7 
-    //int set6[] = {1822,1799,57,23,55}; // 4 
-    //int set5[]  = {1, 2, 3, 3, 2, 1}; // 4 
-    //int set4[] = {5,5 ,5, 5}; // 2
-    //int set3[] = {1,3,2}; // 2
-    //int set1[] = {0,0}; // 0
-    //int size = 2;
-    //int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // 4 a 8 39
-    // subArray(arr,4,8);
-    //A XOR B = C
-    //A = B XOR C
-
-    
-
-    //for(int i = 1; i < size; ++i)
-    //{
-    //    set1[i] ^= set1[i - 1];
-    //}
-
-
-
-    // cout <<  (arr[8] ^ arr[4]) << endl;
-
-    //print(set1, size);
-    func();
-    
-    return 0;
-}
-
-void func()
-{
-    int t = 0; 
+    int t = 0;
+    //N test cases
     cin >> t;
 
     while(t--)
     {
         int n = 0;
+        //n elements per test case
         cin >> n;
         int* arr = new int[n];
+
+        //Recieved elements
         for(int i = 0; i < n; ++i)
             cin >> arr[i];
         
+        // Partial XOR sums
         int count_zero = 0;
         unordered_set<int> repeated;
         for(int i = 1; i < n; ++i)
@@ -69,6 +30,8 @@ void func()
         }
         //cout<<endl;
         repeated.insert(0);
+
+        //Remove repeated elements that may be cleared with XOR
         for(int i = 0; i < n; ++i)
         {
             if(repeated.find(arr[i]) != repeated.end()){
@@ -80,8 +43,14 @@ void func()
        
         cout << n - count_zero << endl;
     }
-
 }
+int main(int argc, char const *argv[])
+{       
+    bureka_algorithm();
+    return 0;
+}
+
+
 
 
 
